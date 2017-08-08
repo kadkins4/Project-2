54ce461b52f7d67ae446f1b5e4d23e14b5f50a55
Kendall <adkins.kendall90@gmail.com>
2017-08-08 08:47:37 -0400

    forgot commits, mvp and more, style needs work

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,29 @@
 class HerosController < ApplicationController
+
+  def index
+    @heros = Hero.all
+  end
+
+  def show
+    @hero = Hero.find(params[:id])
+  end
+
+  def edit
+    # if current_user == admin
+    @team = Team.find(params[:id])
+    @hero = Hero.find(params[:id])
+  end
+
+  def update
+    # if current_user == admin
+    @hero = Hero.find(params[:id])
+    @hero.update!(hero_params)
+    flash[:notice] = "Updated #{@hero.name}"
+  end
+
+private
+  def hero_params
+    params.require(:hero).permit(:name, :primary, :image, :complexity, :description, :playing, :with, :against, :team_id)
+  end
+
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,7 +6,23 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
+    @heros = @team.heros
+    @unpicked = Team.find_by_name(name = "unpicked")
+  end
 
+  def edit
+    @team = Team.find(params[:id])
+    @heros = Hero.all
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    @team.update!(team_params)
+  end
+
+private
+  def team_params
+    params.require(:team).permit(@team.heros)
   end
 
 end
